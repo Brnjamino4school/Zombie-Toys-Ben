68967b1917d3157461a7b228e26e38fe0f58d5dd
Brnjamino4school <benjamin.ransom@ocsbstudent.ca>
2017-05-10 09:33:41 -0400

    Current update Chapter 15 prep

---
+++ ZombieToys/Assets/Navigation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigation : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] float updateDelay = .3f;
+    // Use this for initialization
+    void Reset()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Start () {
+        InvokeRepeating("FollowTarget", 0f, updateDelay);
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(target.position);
+    }
+}
